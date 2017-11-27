2867b876c9a6b838acef799278331400dd2e8baa
BrancartC <HE201330@students.ephec.be>
2017-11-27 17:52:40 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,8 +6,8 @@ public class Main {
    
     public static void main(String[] args) {
     	InterGraph interGraph = new InterGraph();
-        Mastermind game = new Mastermind();
+        //Mastermind game = new Mastermind();
               
-        game.play();
+        //game.play();
     }
 }
