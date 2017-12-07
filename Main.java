955486679775b71a77212ed28c5530af744e3c69
Nassim <he201275@students.ephec.be>
2017-12-07 16:27:46 +0100

    Graphisme

---
+++ Main.java
@@ -0,0 +1,13 @@
+package MasterM;
+import javax.swing.*;
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+    	InterGraph interGraph = new InterGraph();
+        //Mastermind game = new Mastermind();
+              
+        //game.play();
+    }
+}
